7d2bedfa998b69e3d8d8ddb6bd92886467783ed5
unknown <almeida.luan099@gmail.com>
2022-01-30 14:40:09 -0300

    Adicionando metodo generate jwt

--- DevFreela.Infrastructure/service/AuthService.cs
+++ DevFreela.Infrastructure/service/AuthService.cs
@@ -1,12 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using DevFreela.Core.service;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
 
 namespace DevFreela.Infrastructure.service
 {
     public class AuthService : IAuthService
     {
+        private readonly IConfiguration configuration;
+        public AuthService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public string GenerateJwtToken(string email, string role)
         {
-            throw new System.NotImplementedException();
+            var issuer = configuration["Jwt:Issuer"];
+            var audience = configuration["Jwt:Audience"];
+            var key = configuration["Jwt:Key"];
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+             var claims = new List<Claim>
+            {
+                new Claim("userName", email),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials,
+                claims: claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var stringToken = tokenHandler.WriteToken(token);
+
+            return stringToken;
+        
         }
+
+
     }
 }
